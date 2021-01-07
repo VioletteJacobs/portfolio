2f046615a6110b10c87fe0275bdfe391a126adaa
Violette <violette.jacobs@hotmail.com>
2021-01-07 17:58:17 +0100

    typewriter

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+let body= document.body 
+// texte animé
+let textAnimed = document.getElementById('jeSuis');
+console.log(textAnimed);
+
+let jeSuis= new Typewriter(textAnimed)
+.typeString("Je suis développeuse ")
+.changeDelay(40)
+.pauseFor(300)
+.typeString(" Full-stack")
+.pause(1000)
+.deleteChars(10)
+.typeString(" JavaScript")
+.pauseFor(1000)
+.deleteChars(10)
+.typeString(" Php")
+.pauseFor(1000)
+.deleteChars(3)
+.start()
